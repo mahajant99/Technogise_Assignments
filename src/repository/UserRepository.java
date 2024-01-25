fa2477433a2f01cfcaa58f72b67fae1994fb3b10
Tushar Mahajan <mahajant99@gmail.com>
2024-01-25 12:42:23 +0530

    ToDoApplication: Add signin function to user repository
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -4,6 +4,7 @@ import model.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserRepository {
@@ -27,4 +28,22 @@ public class UserRepository {
         }
     }
 
+    public boolean isUserExists(String username, String password) {
+        String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                return resultSet.next();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+    
 }
