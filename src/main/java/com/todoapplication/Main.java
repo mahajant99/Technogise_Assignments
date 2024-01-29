31bbfb60d2e243973191683c2697c3ca3cc6bac5
Tushar Mahajan <mahajant99@gmail.com>
2024-01-29 10:52:09 +0530

    ToDoApplication: Fix working of main code
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- src/main/java/com/todoapplication/Main.java
+++ src/main/java/com/todoapplication/Main.java
@@ -11,40 +11,66 @@ import com.todoapplication.view.TaskUI;
 public class Main {
 
     public static void main(String[] args) {
-        
+
         UserService userService = new UserService(new UserRepository(DatabaseManager.getConnection()));
         TaskService taskService = new TaskService(new TaskRepository(DatabaseManager.getConnection()));
         TaskManager taskManager = new TaskManager(taskService, userService);
         TaskUI taskUI = new TaskUI();
 
+        boolean loggedIn = false;
+        String token = null;
+
         while (true) {
-            taskUI.displayLoginMenu();
-            int input = taskUI.getUserInput();
+            if (!loggedIn) {
+                taskUI.displayLoginMenu();
+                int loginChoice = taskUI.getLoginChoice();
 
-            switch (input) {
-                case 1:
-                    String username = taskUI.getUsername();
-                    String password = taskUI.getPassword();
+                switch (loginChoice) {
+                    case 1:
+                        String username = taskUI.getUsername();
+                        String password = taskUI.getPassword();
 
-                    String token = userService.signIn(username, password);
-                    if (token != null) {
-                        taskUI.displayMessage("Login successful. Token: " + token);
-                        
-                        taskManager.showTaskMenu(taskUI);
-                        int userInput = taskUI.getUserInput();
+                        token = userService.signIn(username, password);
+                        if (token != null) {
+                            taskUI.displayMessage("Login successful. Token: " + token);
+                            loggedIn = true;
+                        } else {
+                            taskUI.displayMessage("Invalid credentials. Please try again.");
+                        }
+                        break;
+                    case 2:
+                        taskManager.signUp(taskUI);
+                        break;
+                    default:
+                        taskUI.displayMessage("Invalid choice. Please try again.");
+                }
+            } else {
+                taskUI.displayTaskListMenu();
+                int taskListChoice = taskUI.getUserInput();
 
-                        String result = taskManager.performTask(userInput, taskUI);
-                        taskUI.displayMessage(result);
-
-                    } else {
-                        taskUI.displayMessage("Invalid credentials. Please try again.");
-                    }
-                    break;
-                case 2:
-                    taskManager.signUp(taskUI);
-                    break;
-                default:
-                    taskUI.displayMessage("Invalid choice. Please try again.");
+                String result;
+                switch (taskListChoice) {
+                    case 1:
+                        result = taskManager.addTask(taskUI);
+                        break;
+                    case 2:
+                        result = taskManager.viewTasks();
+                        break;
+                    case 3:
+                        result = taskManager.editTask(taskUI);
+                        break;
+                    case 4:
+                        result = taskManager.deleteTask(taskUI);
+                        break;
+                    case 5:
+                        result = taskManager.markTaskAsCompleted(taskUI);
+                        break;
+                    case 6:
+                        System.exit(0);
+                    default:
+                        result = "Invalid input. Please try again.";
+                }
+                taskUI.displayMessage(result);
             }
         }
     }
